31d07dd7c2d24556f34431d629d4495ef555d462
thomaspringle <pringle.thomas@gmail.com>
2014-03-06 22:37:39 +0100

    Added Unauthorized exception

--- src/test/java/se/altran/restkurs/webapi/actor/ActorResourceDeleteTest.java
+++ src/test/java/se/altran/restkurs/webapi/actor/ActorResourceDeleteTest.java
@@ -1,22 +1,15 @@
 package se.altran.restkurs.webapi.actor;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
 import org.eclipse.jetty.server.Server;
 import org.junit.After;
 import org.junit.Before;
@@ -32,11 +25,10 @@ import com.google.inject.AbstractModule;
 
 public class ActorResourceDeleteTest {
 
-	private static final String ACTOR_JSON = "actor.json";
 	private Server server;
-	private String uuid = "actor_uuid";
 	private IActorService actorService;
 	private String userToken = "user_token_value";
+	private String unAuthorizedToken = "-";
 	private List<Actor> actors = new ArrayList<>();
 	
 	@Before
@@ -45,7 +37,7 @@ public class ActorResourceDeleteTest {
 		// Mock the ActorService with some test data
 		actorService = mock(IActorService.class);
 		when(actorService.deleteActors(userToken)).thenReturn(actors);
-
+		when(actorService.deleteActors(unAuthorizedToken)).thenThrow(new SecurityException());
 		// Start the server
 		AbstractModule testModule = new DomainModule(actorService);
 		server = AltranREST.startServer(8090, testModule);
@@ -60,10 +52,6 @@ public class ActorResourceDeleteTest {
 		httpDelete.setHeader("Accept", "application/json");
 		httpDelete.setHeader("Authorization", userToken);
 
-//		// Define data to be posted
-//		String data = getActorJsonFromFile();
-//		httpPost.setEntity(new StringEntity(data, ContentType.create("application/json")));
-
 		// Execute method and receive response
 		HttpResponse response = httpHelper.executeMethod(httpDelete);
 		
@@ -74,6 +62,25 @@ public class ActorResourceDeleteTest {
 		verify(actorService).deleteActors(userToken);
 	}
 
+	@Test
+	public void testActors_POST_ReturnUnAuthorized() throws Exception {
+		
+		// Create POST command to /webapi/actors
+		HttpHelper httpHelper = new HttpHelper("127.0.0.1", 8090);
+		HttpDelete httpDelete = new HttpDelete("/webapi/actors");
+		httpDelete.setHeader("Accept", "application/json");
+		httpDelete.setHeader("Authorization", unAuthorizedToken);
+
+		// Execute method and receive response
+		HttpResponse response = httpHelper.executeMethod(httpDelete);
+		
+		// Verify that the correct status code has been set
+		int statusCode = response.getStatusLine().getStatusCode();
+		assertEquals("Expected Status Code 401 - Unauthorized", 401, statusCode);	
+		
+		verify(actorService).deleteActors(unAuthorizedToken);
+	}
+	
 	
 	@After
 	public void tearDown() {
